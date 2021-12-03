29499b052b4e044084974a24b8ebc12bfb4a1ecd
akaya1023 <an7099265@gmail.com>
2021-12-03 17:31:05 +0900

    conect to sqlite3

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
 
-/*const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('/mydata.db',(err) =>{
-if(err){
-   console.log(error.message)
-}
-});*/
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('mydata.sqlite3');
+
 
 app.use(express.static('public'));
 
@@ -27,6 +24,23 @@ app.get('/new',(req,res) => {
 app.get('/create',(req,res) =>{
    console.log(req.body.itemName);
 });
+
+
+app.get('/', (req,res,next) =>{
+db.serialize(() =>{
+   db.all("select * from ShoppingList",(err,rows) =>{
+      if(err){
+         const data = {
+            title:'Shoppling List App',
+            content: rows
+         }
+         res.render('views/index.ejs',data);
+      }
+   });
+});
+});
+
+
 
 app.post('/delete',(req,res) =>{
    res.redirect('/index');
