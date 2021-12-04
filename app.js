838b2ff2ac47acf9c854b40598b66b8562f0ae2f
akaya1023 <an7099265@gmail.com>
2021-12-04 23:10:50 +0900

    created delete form

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const sqlite3 = require('sqlite3').verbose();//import the sqlite3 module//
 
 app.use(express.static('public'));
 
-app.use(express.urlencoded({extended: false}));
-
+app.use(express.urlencoded({ extended: false }));
+///get values from submitted forms
 
 //create a Database object://
 const db = new sqlite3.Database('mydata.sqlite3', (err) => {
    if (err) {
-     return console.error(err.message);
+      return console.error(err.message);
    }
    console.log('Connected to the in-mydata.sqlite3 SQlite database.');
- });
+});
 
 
 //  //db.close((err) => {
@@ -25,34 +25,51 @@ const db = new sqlite3.Database('mydata.sqlite3', (err) => {
 //  });//
 
 
-app.get('/',(req, res) => {
-res.render('top.ejs');
+app.get('/', (req, res) => {
+   res.render('top.ejs');
 });
 
 
-app.get('/index',(req,res) => {
+app.get('/index', (req, res) => {
    db.all(     //call back all of datas//
-'SELECT*FROM ShoppingLIst',
-(error,results) => {
-   console.log(results);//output on the terminal where the server is running.//
-   res.render('index.ejs',{ShoppingLIst: results}); //Pass an object //
+      'SELECT*FROM ShoppingList',
+      (error, results) => {
+         console.log(results);//output on the terminal where the server is running.//
+         res.render('index.ejs', { ShoppingList: results }); //Pass an object //
+      }
+   );
+
+});
+
+app.get('/new', (req, res) => {
+   res.render('new.ejs'); // Specify the view file within the route//
+});
+
+app.post('/create', (req, res) => {
+   db.all(
+      'INSERT INTO ShoppingList(name) VALUES(?)',
+      [req.body.itemName],
+      (error, results) => {
+         res.redirect('/index');
+      }
+   );
+});
+
+
+app.post('/delete/:id', (req, res) => {
+   db.all(
+    'DELETE FROM ShoppingList WHERE id = ?',
+    [req.params.id],
+    (error,results) =>{
+       res.redirect('/index');
+    }
+   );
+   });
+app.get('/edit/:id',(req,res) =>{
+   res.render('edit.ejs');
 }
 );
 
-});
-
-app.get('/new',(req,res) => {
-    res.render('new.ejs');
-});
-
-app.get('/create',(req,res) =>{
-   console.log(req.body.itemName);
-});
-
-
-app.post('/delete',(req,res) =>{
-   res.redirect('/index');
-});
 /*Node.js 2 → SQL */
 
 app.listen(3000);
